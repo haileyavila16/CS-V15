d59066edd259d18a1364065b88a19a12f16bde11
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-09-13 13:41:08 -0700

    Assignment 0-1

---
+++ array1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int N = 10;
+	int numbers[N] = { 5,7,2,4,6,7,10,1,3,9};
+
+	for( )
+	{
+        if(int i=1; i<N-1; i++)
+        {
+            if ((numbers[i-1] < numbers[i]))
+        }
+
+	}
+
+}
---
+++ ex1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This Program is ex1.cpp \n" << endl;
+}
