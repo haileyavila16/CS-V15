70b997ab0bbb7b1098e7c470d7d7bfdc1eedd09d
haileyavila16 <77656592+haileyavila16@users.noreply.github.com>
2021-09-13 14:55:24 -0700

    array1.cpp

---
+++ Array Assignments/array1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int N = 10;
+	int numbers[N] = { 5,7,2,4,6,7,10,1,3,9};
+
+	for(int i=1; i<N-1; i++ )
+	{
+		if (( numbers[i-1] < numbers[i] ) &&  (numbers[i] < numbers[i+1] ))
+			cout << numbers[i] << " has the increasing order\n";
+	}
+
+}
